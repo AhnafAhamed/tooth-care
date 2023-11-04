7b609e3414fe02c00fcf3d4d692150b3162ea148
Ahnaf Ahamed <ahnafahamed4@gmail.com>
2023-11-04 23:04:17 +0530

    add mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import "@mantine/core/styles.css";
 import "./App.css";
+import { MantineProvider, Button, createTheme } from "@mantine/core";
 
 function App() {
+  const theme = createTheme({
+    shadows: {
+      md: "1px 1px 3px rgba(0, 0, 0, .25)",
+      xl: "5px 5px 3px rgba(0, 0, 0, .25)",
+    },
+  });
   return (
-    <>
+    <MantineProvider theme={theme}>
       <h1>Hello World!</h1>
-    </>
+      <Button>Hello</Button>
+    </MantineProvider>
   );
 }
 

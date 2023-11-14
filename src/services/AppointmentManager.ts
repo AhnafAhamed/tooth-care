b8fee160ea070c30885990b74cda3fcb09c37797
Ahnaf Ahamed <ahnafahamed4@gmail.com>
2023-11-14 09:39:35 +0530

    Refactor AppointmentManager to use Map instead of array

--- src/services/AppointmentManager.ts
+++ src/services/AppointmentManager.ts
@@ -1,38 +1,26 @@
 import { Appointment } from "./Appointment";
-
 export class AppointmentManager {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
+  private appointments: Map<number, Appointment> = new Map();
 
   public addAppointment(appointment: Appointment): void {
-    this.appointments.push(appointment);
+    this.appointments.set(appointment.getId(), appointment);
+  }
+
+  public getAppointmentById(id: number): Appointment | undefined {
+    return this.appointments.get(id);
+  }
+
+  public removeAppointment(id: number): void {
+    this.appointments.delete(id);
   }
 
   public getAllAppointments(): Appointment[] {
-    return this.appointments;
+    return Array.from(this.appointments.values());
   }
 
-  public getAppointmentById(appointmentId: number): Appointment | undefined {
-    return this.appointments.find(
-      (appointment) => appointment.getId() === appointmentId
-    );
-  }
-
-  public payAppointment(appointmentId: number): void {
-    const appointment = this.appointments.find(
-      (appointment) => appointment.getId() === appointmentId
-    );
+  public payAppointment(id: number): void {
+    const appointment = this.appointments.get(id);
     if (!appointment) return;
     appointment.payTreatment();
-  }
-
-  public removeAppointment(appointment: Appointment): void {
-    const index = this.appointments.indexOf(appointment);
-    if (index !== -1) {
-      this.appointments.splice(index, 1);
-    }
   }
 }
